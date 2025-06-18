d9ba756032fd04ea2b47fb8ce3e42a2c30df534c
gaqlaxy <v4vripper@gmail.com>
2025-06-18 06:43:15 +0530

    Modified FAQ and Testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -212,7 +212,7 @@ const Testimonials = () => {
   return (
     <section className="py-12 sm:py-20 overflow-hidden ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl sm:text-5xl font-bold text-center mb-8 sm:mb-12">
+        <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
           Hear from our customers
         </h2>
 
